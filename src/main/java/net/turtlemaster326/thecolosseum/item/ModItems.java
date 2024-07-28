1eac866dbdae9f5db2efb2332ebfc2ddcc8e490f
Turtlemaster326 <66545988+HorizonKnight@users.noreply.github.com>
2024-07-27 19:03:58 -0500

    Added black opal

---
+++ src/main/java/net/turtlemaster326/thecolosseum/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.turtlemaster326.thecolosseum.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.turtlemaster326.thecolosseum.TheColosseum;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TheColosseum.MOD_ID);
+
+    public static final RegistryObject<Item> BLACKOPAL = ITEMS.register("black_opal",
+            () -> new Item(new Item.Properties()));
+
+
+
+
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
